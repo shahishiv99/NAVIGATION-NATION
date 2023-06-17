f40550c3dfc57d9a3b06775aa1c63c13dab9ebd6
Shivam Sharma <shahishiv99@gmail.com>
2023-06-18 03:26:25 +0530

    Complete

--- script.js
+++ script.js
@@ -4,23 +4,35 @@ const nav2 = document.getElementById("nav-b");
 const nav3 = document.getElementById("nav-c");
 const nav4 = document.getElementById("nav-d");
 const nav5 = document.getElementById("nav-e");
+
 const overlay = document.getElementById("overlay");
+const nevItems = [nav1, nav2, nav3, nav4, nav5];
+
+function navAnimation(dir1, dir2) {
+  nevItems.forEach((nav, i) => {
+    console.log(`slide-${dir1}-${i + 1}`, `slide-${dir2}-${i + 1}`);
+    nav.classList.replace(`slide-${dir1}-${i + 1}`, `slide-${dir2}-${i + 1}`);
+  });
+}
 
 function toggleNav() {
   menuBar.classList.toggle("change");
   overlay.classList.toggle("overlay-active");
   if (overlay.classList.contains("overlay-active")) {
-    overlay.classList.add("overlay-slide-right");
-    overlay.classList.remove("overlay-slide-left");
+    overlay.classList.replace("overlay-slide-left", "overlay-slide-right");
+    // Animation
+    navAnimation("out", "in");
+
+    // Animation
   } else {
-    overlay.classList.add("overlay-slide-left");
-    overlay.classList.remove("overlay-slide-right");
+    overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
+    // Animation
+    navAnimation("in", "out");
   }
 }
 
 menuBar.addEventListener("click", toggleNav);
-nav1.addEventListener("click", toggleNav);
-nav2.addEventListener("click", toggleNav);
-nav3.addEventListener("click", toggleNav);
-nav4.addEventListener("click", toggleNav);
-nav5.addEventListener("click", toggleNav);
+
+nevItems.forEach((nav) => {
+  nav.addEventListener("click", toggleNav);
+});
